5e9ac24e11f6d573ca9a5805a647add09d3bdd3a
enzoribas <enzortorres2005@gmail.com>
2024-08-06 00:00:02 -0300

    update

--- pythonEXCEV/ex115/sistema.py
+++ pythonEXCEV/ex115/sistema.py
@@ -14,12 +14,12 @@ while True:
     if resposta == 1:
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('          \033[4;31mNOVO CADASTRO\033[m')
+        cabecalho('          \033[4;31mNOVO CADASTRO\033[m')
         nome = str(input('\033[35mNome: '))
         idade = leiaInt('Idade: \033[m')
         cadastrar(arq, nome, idade)
     elif resposta == 3:
-        cabeçalho('      \033[35mSaindo do sistema... Até logo!\033[m')
+        cabecalho('      \033[35mSaindo do sistema... Até logo!\033[m')
         break
     else:
         print('\033[31mERRO! Digite uma opção válida!\033[m')
