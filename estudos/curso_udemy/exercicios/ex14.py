31ef9fa77ac0f92b604be34b4c226add23859abe
Enzo Ribas <enzortorres2005@gmail.com>
2025-03-15 11:10:27 -0300

    studying python

--- estudos/curso_udemy/exercicios/ex14.py
+++ estudos/curso_udemy/exercicios/ex14.py
@@ -6,3 +6,15 @@ def multiplicar(*args):
     print(total)
     
 multiplicar(1,2,3,4)
+
+
+def par_impar(number):
+    try:
+        if number % 2 == 0:
+            return f"{number} é par."
+        return f"{number} é ímpar."
+    except:
+        return("Digite apenas números.")
+
+
+print(par_impar(3))
