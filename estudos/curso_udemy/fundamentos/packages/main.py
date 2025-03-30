e044b3f307d79e019f608d4d5df1250f66d687cc
Enzo Ribas <enzortorres2005@gmail.com>
2025-03-30 14:45:51 -0300

    studying packages

---
+++ estudos/curso_udemy/fundamentos/packages/main.py
@@ -0,0 +1,17 @@
+# from sys import path
+# print(*path, sep='\n')
+
+import main_package.module
+print(main_package.module.soma_do_modulo(2,3)) 
+
+from main_package import module
+print(module.soma_do_modulo(2,3)) 
+
+from main_package.module import soma_do_modulo
+print(soma_do_modulo(2,3))
+
+# ! MÁ PRÁTICA
+from main_package.module import * 
+print(soma_do_modulo(2,3))
+
+
---
+++ estudos/curso_udemy/fundamentos/packages/main_package/module.py
@@ -0,0 +1,9 @@
+__all__ = [ # ? Oque vai ser importado no from main_package import *
+    'variavel',
+    'soma_do_produto'
+]
+
+variavel = 'Alguma coisa'
+
+def soma_do_modulo(x, y):
+    return x + y
