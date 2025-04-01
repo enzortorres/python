08f0ea7abfdfb33ab4fbf54823681af9df492ec6
Enzo Ribas <enzortorres2005@gmail.com>
2025-04-01 01:47:34 -0300

    studying decorators

---
+++ estudos/curso_udemy/fundamentos/decoradoras/main.py
@@ -0,0 +1,32 @@
+# # | Funções decoradoras e decoradores
+# ? Decorar = Adicionar / Remover/ Restringir / Alterar
+# ? Funções decoradoras são funções que decoram outras funções
+# ? Decoradores são usados para fazer o Python usar as funções decoradoras em outras funções.
+# ? Decoradores são "Syntax Sugar" (Açúcar sintático)
+
+
+def criar_funcao(func): # ! Função decoradora, criar uma função internada sem execução, somente retornada
+    def interna(*args, **kwargs):
+        print("Vou te decorar")
+        
+        for arg in args: # > Dentro da função interna eu posso (Adicionar / Remover/ Restringir / Alterar) antes de dar o resultado
+            is_string(arg)
+        resultado = func(*args, **kwargs)
+        
+        print(f"O seu resultado foi {resultado}")
+        print("Ok, agora você foi decorada")
+        
+        return resultado
+    return interna
+
+@criar_funcao
+def inverte_string(string):
+    return string[::-1]
+
+def is_string(param):
+    if not isinstance(param, str):
+        raise TypeError("Param deve ser uma string")
+
+
+invertida = inverte_string('123')
+print(invertida)
--- estudos/curso_udemy/fundamentos/local_nonlocal/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def fat(n):
-    if n > 1:
-        return n * fat(n-1)
-    else: 
-        return n
-    
-print(fat(5))
