2fd9a3c30b8263ebd624c3a687d0a291e8c81b3d
enzortorres <enzortorres2005@gmail.com>
2025-07-23 23:36:06 -0300

    studying pandas how to assign values

--- estudos/pandas/fundamentos/teste.py
+++ estudos/pandas/fundamentos/teste.py
@@ -1,13 +1,25 @@
-import pandas as pd
+# import pandas as pd
 
-data = pd.read_csv('C:\\Users\\enzor\\OneDrive\\Desktop\\Estudos\\python\\estudos\\pandas\\datasets\\2004-2021.csv', sep=";")
-print(data)
+# data = pd.read_csv('C:\\Users\\enzor\\OneDrive\\Desktop\\Estudos\\python\\estudos\\pandas\\datasets\\2004-2021.csv', sep=";")
+# print(data)
 
-data.info()
+# data.info()
 
-df = pd.DataFrame({
-    'nome': ['Enzo', 'Vitor'],
-    'idade': [20, 20]
-})
+# df = pd.DataFrame({
+#     'nome': ['Enzo', 'Vitor'],
+#     'idade': [20, 20]
+# })
 
-print(df)
+# print(df)
+
+# import pandas as pd 
+
+# data = pd.DataFrame({'PRODUTO': ['Gasolina', 'Etanol', 'Diesel']})
+
+# produto_view = data['PRODUTO'] # ! NÃO passa mais como refêrencia (ponteiro)
+# produto_copy = data['PRODUTO'].copy()
+
+# data['PRODUTO'] = 'Combustível'
+
+# print(produto_view)
+# print(produto_copy)
