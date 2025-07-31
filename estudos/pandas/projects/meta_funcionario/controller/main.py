9c4583b270ca713029116aeeae48d0a9c04ba3d8
Enzo Ribas Torres <enzortorres2005@gmail.com>
2025-07-31 00:17:41 -0300

    studying pandas

--- estudos/pandas/projects/meta_funcionario/controller/main.py
+++ estudos/pandas/projects/meta_funcionario/controller/main.py
@@ -1,3 +1,17 @@
 import pandas as pd
+from pathlib import Path
 
-dataset = pd.read_excel
+DATASET_PATH = Path(__file__).parent.parent
+DATASET_FILE = DATASET_PATH / 'datasets/vendas_funcs.xlsx'
+
+dataset = pd.read_excel(DATASET_FILE)
+
+print('Bateram a meta:')
+for i in range(len(dataset)):
+    if dataset.loc[i, 'Vendas'] > 18000:
+        print(f'Nome: {dataset.loc[i, "Nome"]} - Vendas: {dataset.loc[i, "Vendas"]}')
+
+print('\nVendas abaixo do mínimo:')
+for i in range(len(dataset)):
+    if dataset.loc[i, 'Vendas'] < 7000:
+        print(f'Nome: {dataset.loc[i, "Nome"]} - Vendas: {dataset.loc[i, "Vendas"]}')
