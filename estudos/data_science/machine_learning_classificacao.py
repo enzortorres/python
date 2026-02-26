e3ef85af38f4bd6c79dc6a4a829aaa6cb1188aa3
Enzo Ribas <enzortorres2005@gmail.com>
2026-02-26 12:06:58 -0300

    study(DS), pandas, studying about machine learning classification

---
+++ estudos/data_science/machine_learning_classificacao.py
@@ -0,0 +1,87 @@
+import pandas as pd
+
+data = pd.read_csv('titanic_train.csv')
+
+# > Verificando as informações da base
+# print(data.info())
+
+# > Eliminando a coluna 'Cabin'
+data = data.drop('Cabin', axis=1)
+
+# > Verificando se foi removido
+# print(data.info())
+
+# > Eliminar linhas com valores vazios
+data.dropna(inplace=True) 
+
+# > Selecionando as colunas com o tipo "object"
+objects_data = data.dtypes[data.dtypes.values == 'object'].index
+
+# > Eliminando essas colunas
+data = data.drop(objects_data, axis=1)
+
+# ! Utilizando Scikit-Learn
+
+print("\nDados de treino\n")
+
+# > Definindo o X e o y para o treino
+x = data.drop('Survived', axis=1)
+y = data.Survived
+
+
+# | Utilizando KNN
+from sklearn.neighbors import KNeighborsClassifier
+
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(x, y)
+
+# > Avaliando o modelo
+print(neigh.score(x, y))
+
+# | Utilizando Tree
+from sklearn import tree
+clf_tree = tree.DecisionTreeClassifier()
+clf_tree = clf_tree.fit(x, y)
+
+# > Avaliando o modelo
+print(clf_tree.score(x,y))
+
+# | Utilizando regressão logística
+
+from sklearn.linear_model import LogisticRegression
+
+clf_log = LogisticRegression(random_state=0, max_iter=1000).fit(x, y)
+print(clf_log.score(x, y))
+
+data_test = pd.read_csv("titanic_test.csv")
+
+# ! Fazendo os mesmos tratamento para os dados de teste
+
+print("\nDados de teste\n")
+
+# > Eliminar a coluna `Cabin`
+data_test = data_test.drop('Cabin', axis=1)
+
+# > Eliminar linhas com valores vazios
+data_test.dropna(inplace=True) 
+
+# > Selecionando as colunas com o tipo `object`
+objects_data = data_test.dtypes[data_test.dtypes.values == 'object'].index
+
+# > Eliminando essas colunas
+data_test = data_test.drop(objects_data, axis=1) 
+
+# > Separando X e y da base de teste
+x_test = data_test.drop('Survived', axis=1)
+y_test = data_test.Survived
+
+neigh_test = KNeighborsClassifier(n_neighbors=3)
+
+# > Fazendo a predição com o KNN
+pred_KNN = neigh.predict(x_test)
+
+# > Predict com a Árvore de Decisão
+pred_Tree = clf_tree.predict(x_test)
+
+# > Predict com Regressão Logística
+pred_Log = clf_log.predict(x_test)
