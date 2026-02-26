a810af3f615a39c3515ba301bcb528adcedbc415
Enzo Ribas <enzortorres2005@gmail.com>
2026-02-26 13:56:30 -0300

    study(DC): pandas, studying about confusion matrix, accuracy, precision and recall score

--- estudos/data_science/machine_learning_classificacao.py
+++ estudos/data_science/machine_learning_classificacao.py
@@ -40,7 +40,7 @@ print(neigh.score(x, y))
 
 # | Utilizando Tree
 from sklearn import tree
-clf_tree = tree.DecisionTreeClassifier()
+clf_tree = tree.DecisionTreeClassifier(random_state=0)
 clf_tree = clf_tree.fit(x, y)
 
 # > Avaliando o modelo
@@ -79,9 +79,95 @@ neigh_test = KNeighborsClassifier(n_neighbors=3)
 
 # > Fazendo a predição com o KNN
 pred_KNN = neigh.predict(x_test)
+print(neigh.score(x, y))
 
 # > Predict com a Árvore de Decisão
 pred_Tree = clf_tree.predict(x_test)
+print(clf_tree.score(x,y))
 
 # > Predict com Regressão Logística
 pred_Log = clf_log.predict(x_test)
+print(clf_log.score(x, y))
+
+
+
+
+
+# | Usando matriz de confusão
+# url: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+
+print("\nMatriz de Confusão\n")
+
+# > Importando a matriz de confusão
+from sklearn.metrics import confusion_matrix
+
+# > Verificando a matriz para o KNN
+print("KNN")
+print(confusion_matrix(y_test, pred_KNN))
+
+print("TREE")
+# > Verificando a matriz para a Árvore
+print(confusion_matrix(y_test, pred_Tree))
+
+print("LOG")
+# > Verificando a matriz para a Regressão Logística
+print(confusion_matrix(y_test, pred_Log))
+
+# | Usando Acurácia
+# url: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.accuracy_score.html
+
+print("\nAcurácia\n")
+
+# > Importando a métrica de acurácia
+from sklearn.metrics import accuracy_score
+
+# > Verificando a acurácia para o KNN
+print("KNN")
+print(accuracy_score(y_test, pred_KNN))
+
+print("TREE")
+# > Verificando a acurácia para a Árvore
+print(accuracy_score(y_test, pred_Tree))
+
+print("LOG")
+# > Verificando a acurácia para a Regressão Logística
+print(accuracy_score(y_test, pred_Log))
+
+# | Usando Precisão
+
+print("\nPrecisão\n")
+
+# > Importando a métrica de precisão
+from sklearn.metrics import precision_score
+
+# > Verificando a precisão para o KNN
+print("KNN")
+print(precision_score(y_test, pred_KNN))
+
+print("TREE")
+# > Verificando a precisão para a Árvore
+print(precision_score(y_test, pred_Tree))
+
+print("LOG")
+# > Verificando a precisão para a Regressão Logística
+print(precision_score(y_test, pred_Log))
+
+# | Usando o Recall
+# url: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.recall_score.html
+
+print("\nRecall\n")
+
+# > Importando a métrica do recall
+from sklearn.metrics import recall_score
+
+# > Verificando o recall para o KNN
+print("KNN")
+print(recall_score(y_test, pred_KNN))
+
+print("TREE")
+# > Verificando o recall para a Árvore
+print(recall_score(y_test, pred_Tree))
+
+print("LOG")
+# > Verificando o recall para a Regressão Logística
+print(recall_score(y_test, pred_Log))
